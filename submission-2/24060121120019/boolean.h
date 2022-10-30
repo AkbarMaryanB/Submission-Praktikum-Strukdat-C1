b989af48a947e3afafb4a28fc4488ae97a0a0871
AkbarMaryanB <100180116+AkbarMaryanB@users.noreply.github.com>
2022-10-31 05:38:49 +0700

    Add files via upload

---
+++ submission-2/24060121120019/boolean.h
@@ -0,0 +1,12 @@
+/*Nama File : boolean.h*/ 
+/*Deskripsi : Header fungsi boolean */ 
+/* NIM/Nama  : 24060121120019/Akbar Maryan Bagaskara*/
+/* Tanggal   : 29 oktober 2022*/
+
+/* Kamus */ 
+#ifndef boolean_H 
+#define boolean_H 
+#define true 1 
+#define false 0 
+#define boolean unsigned char 
+#endif
